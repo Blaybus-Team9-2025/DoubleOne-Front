83a01a189612669c4fe9110bbe23919106c498a2
teyeong <taetae0202@naver.com>
2025-02-13 18:11:11 +0900

    feat: 요양사 컨택 페이지 퍼블리싱

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,195 @@
+import { useNavigate, useParams } from 'react-router-dom';
+import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+import chevron from '../assets/chevron-right.png';
+import Card from '../components/_common/Card';
+
+const Contact = () => {
+  const params = useParams();
+  const { id } = params;
+
+  const nav = useNavigate();
+
+  const [select, setSelect] = useState([]);
+
+  const handleSelect = (id) => {
+    setSelect((prev) =>
+      prev.includes(id) ? prev.filter((it) => it !== id) : [...prev, id]
+    );
+  };
+
+  return (
+    <div>
+      <Card bg="green">
+        <p>김ㅇㅇ 어르신</p>
+        <p>서울시 관악구 대학동</p>
+        <p>현재 3명 매칭중</p>
+      </Card>
+      <ListDiv>
+        <Title>요양보호사를 선택해주세요</Title>
+        <ItemWrapper>
+          <Item
+            bg={select.includes(1) ? 'blue' : undefined}
+            onClick={() => nav(`/care/${1}`)}
+          >
+            <CheckBox
+              bg={select.includes(1) ? 'blue' : undefined}
+              onClick={(e) => {
+                e.stopPropagation();
+                handleSelect(1);
+              }}
+            >
+              <Box bg={select.includes(1) ? 'blue' : undefined} />
+            </CheckBox>
+            <TextDiv>
+              <div>
+                <p>김ㅇㅇ</p>
+                <p>서울시 관악구 대학동</p>
+                <p>서울복지센터 7년 근무</p>
+              </div>
+              <img src={chevron} />
+            </TextDiv>
+          </Item>
+          <Item
+            bg={select.includes(2) ? 'blue' : undefined}
+            onClick={() => nav(`/care/${2}`)}
+          >
+            <CheckBox
+              bg={select.includes(2) ? 'blue' : undefined}
+              onClick={(e) => {
+                e.stopPropagation();
+                handleSelect(2);
+              }}
+            >
+              <Box bg={select.includes(2) ? 'blue' : undefined} />
+            </CheckBox>
+            <TextDiv>
+              <div>
+                <p>김ㅇㅇ</p>
+                <p>서울시 관악구 대학동</p>
+                <p>서울복지센터 7년 근무</p>
+              </div>
+              <img src={chevron} />
+            </TextDiv>
+          </Item>
+          <Item
+            bg={select.includes(3) ? 'blue' : undefined}
+            onClick={() => nav(`/care/${3}`)}
+          >
+            <CheckBox
+              bg={select.includes(3) ? 'blue' : undefined}
+              onClick={(e) => {
+                e.stopPropagation();
+                handleSelect(3);
+              }}
+            >
+              <Box bg={select.includes(3) ? 'blue' : undefined} />
+            </CheckBox>
+            <TextDiv>
+              <div>
+                <p>김ㅇㅇ</p>
+                <p>서울시 관악구 대학동</p>
+                <p>서울복지센터 7년 근무</p>
+              </div>
+              <img src={chevron} />
+            </TextDiv>
+          </Item>
+          <button>더보기</button>
+        </ItemWrapper>
+      </ListDiv>
+      <MsgBtn>메시지 보내기</MsgBtn>
+    </div>
+  );
+};
+
+const ListDiv = styled.div`
+  margin-top: 35px;
+  display: flex;
+  flex-direction: column;
+  gap: 14px;
+`;
+
+const Title = styled.p`
+  font-size: 18px;
+  font-weight: 700;
+`;
+
+const ItemWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 14px;
+  button {
+    font-size: 14px;
+    text-decoration-line: underline;
+    cursor: pointer;
+    margin: 30px 0;
+  }
+`;
+
+const Item = styled.div`
+  width: 100%;
+  height: 98px;
+  box-shadow: var(--shadow);
+  background-color: ${(props) =>
+    props.bg ? 'var(--' + props.bg + ')' : '#ffffff'};
+  display: flex;
+  align-items: center;
+  gap: 25px;
+  padding: 15px;
+  border-radius: 15px;
+  cursor: pointer;
+`;
+
+const CheckBox = styled.div`
+  width: 34px;
+  height: 34px;
+  background-color: ${(props) => (props.bg ? '#ffffff' : '#d9d9d9')};
+  border-radius: 5px;
+  margin-left: 15px;
+  flex-shrink: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Box = styled.div`
+  width: 22px;
+  height: 22px;
+  background-color: ${(props) =>
+    props.bg ? 'var(--' + props.bg + ')' : '#d9d9d9'};
+  border-radius: 2px;
+`;
+
+const TextDiv = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  p {
+    font-size: 12px;
+  }
+  p:first-child {
+    font-size: 18px;
+    font-weight: 700;
+    margin-bottom: 4px;
+  }
+  img {
+    width: 24px;
+    height: 24px;
+  }
+`;
+
+const MsgBtn = styled.button`
+  width: 100%;
+  margin-bottom: 35px;
+  background-color: var(--blue);
+  font-size: 18px;
+  font-weight: 700;
+  padding: 18px 0;
+  border-radius: 15px;
+  box-shadow: var(--shadow);
+  cursor: pointer;
+`;
+
+export default Contact;
