a177f6e6eca082ee6d77eaa037a1bbda908e2953
teyeong <taetae0202@naver.com>
2025-02-15 00:42:13 +0900

    refactor: 다중 선택 기능 및 버튼 제거

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,24 +1,14 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import styled from 'styled-components';
-import { useEffect, useState } from 'react';
 import chevron from '../assets/chevron-right.png';
 import Card from '../components/_common/Card';
 import Header from '../components/_common/Header';
-import SquareButton from '../components/_common/SquareButton';
 
 const Contact = () => {
   const params = useParams();
   const { id } = params;
 
   const nav = useNavigate();
-
-  const [select, setSelect] = useState([]);
-
-  const handleSelect = (id) => {
-    setSelect((prev) =>
-      prev.includes(id) ? prev.filter((it) => it !== id) : [...prev, id]
-    );
-  };
 
   return (
     <div style={{ marginTop: '100px' }}>
@@ -31,19 +21,8 @@ const Contact = () => {
       <ListDiv>
         <Title>요양보호사를 선택해주세요</Title>
         <ItemWrapper>
-          <Item
-            bg={select.includes(1) ? 'blue' : undefined}
-            onClick={() => nav(`/contact/${id}/${1}`)}
-          >
-            <CheckBox
-              bg={select.includes(1) ? 'blue' : undefined}
-              onClick={(e) => {
-                e.stopPropagation();
-                handleSelect(1);
-              }}
-            >
-              <Box bg={select.includes(1) ? 'blue' : undefined} />
-            </CheckBox>
+          <Item onClick={() => nav(`/contact/${id}/${1}`)}>
+            <CheckBox />
             <TextDiv>
               <div>
                 <p>김ㅇㅇ</p>
@@ -53,19 +32,8 @@ const Contact = () => {
               <img src={chevron} />
             </TextDiv>
           </Item>
-          <Item
-            bg={select.includes(2) ? 'blue' : undefined}
-            onClick={() => nav(`/contact/${id}/${2}`)}
-          >
-            <CheckBox
-              bg={select.includes(2) ? 'blue' : undefined}
-              onClick={(e) => {
-                e.stopPropagation();
-                handleSelect(2);
-              }}
-            >
-              <Box bg={select.includes(2) ? 'blue' : undefined} />
-            </CheckBox>
+          <Item onClick={() => nav(`/contact/${id}/${2}`)}>
+            <CheckBox />
             <TextDiv>
               <div>
                 <p>김ㅇㅇ</p>
@@ -75,19 +43,8 @@ const Contact = () => {
               <img src={chevron} />
             </TextDiv>
           </Item>
-          <Item
-            bg={select.includes(3) ? 'blue' : undefined}
-            onClick={() => nav(`/contact/${id}/${3}`)}
-          >
-            <CheckBox
-              bg={select.includes(3) ? 'blue' : undefined}
-              onClick={(e) => {
-                e.stopPropagation();
-                handleSelect(3);
-              }}
-            >
-              <Box bg={select.includes(3) ? 'blue' : undefined} />
-            </CheckBox>
+          <Item onClick={() => nav(`/contact/${id}/${3}`)}>
+            <CheckBox />
             <TextDiv>
               <div>
                 <p>김ㅇㅇ</p>
@@ -99,9 +56,6 @@ const Contact = () => {
           </Item>
         </ItemWrapper>
       </ListDiv>
-      <SquareButton mb={35} mt={35} color="blue">
-        메시지 보내기
-      </SquareButton>
     </div>
   );
 };
@@ -129,8 +83,6 @@ const Item = styled.div`
   width: 100%;
   height: 98px;
   box-shadow: var(--shadow);
-  background-color: ${(props) =>
-    props.bg ? 'var(--' + props.bg + ')' : '#ffffff'};
   display: flex;
   align-items: center;
   gap: 25px;
@@ -142,21 +94,13 @@ const Item = styled.div`
 const CheckBox = styled.div`
   width: 34px;
   height: 34px;
-  background-color: ${(props) => (props.bg ? '#ffffff' : '#d9d9d9')};
+  background-color: #d9d9d9;
   border-radius: 5px;
   margin-left: 15px;
   flex-shrink: 0;
   display: flex;
   justify-content: center;
   align-items: center;
-`;
-
-const Box = styled.div`
-  width: 22px;
-  height: 22px;
-  background-color: ${(props) =>
-    props.bg ? 'var(--' + props.bg + ')' : '#d9d9d9'};
-  border-radius: 2px;
 `;
 
 const TextDiv = styled.div`
