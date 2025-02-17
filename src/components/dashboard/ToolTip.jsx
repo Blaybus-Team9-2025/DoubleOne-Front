deec80703198a97e951a1b748eb29d7e8ebd936b
teyeong <taetae0202@naver.com>
2025-02-17 20:24:09 +0900

    feat: 툴팁 구현

---
+++ src/components/dashboard/ToolTip.jsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components';
+
+import info from '../../assets/info.png';
+import { useState } from 'react';
+
+const ToolTip = ({ text }) => {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(true);
+    setTimeout(() => setIsClicked(false), 5000);
+  };
+
+  return (
+    <Div onClick={handleClick}>
+      <img src={info} />
+      {isClicked && (
+        <TipBox>
+          <p>{text}</p>
+        </TipBox>
+      )}
+    </Div>
+  );
+};
+
+const Div = styled.div`
+  width: 16px;
+  height: 16px;
+  position: relative;
+  img {
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+const TipBox = styled.div`
+  width: max-content;
+  position: absolute;
+  top: -40px;
+  transform: translateX(-45%);
+  border-radius: 10px;
+  background-color: rgba(158, 158, 158, 0.2);
+  padding: 2px;
+  p {
+    color: #4d4d4d;
+    font-size: 12px;
+    font-weight: 600;
+    white-space: pre-line;
+  }
+`;
+
+export default ToolTip;
