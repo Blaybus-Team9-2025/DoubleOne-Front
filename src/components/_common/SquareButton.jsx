4a98a4daa35ac8d2a3e4d4a9632345aa52c7cb9b
teyeong <taetae0202@naver.com>
2025-02-17 22:45:01 +0900

    refactor: SquareButton color 적용 방식 변경

--- src/components/_common/SquareButton.jsx
+++ src/components/_common/SquareButton.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 const SquareButton = ({ children, onClick, mt, mb, color }) => {
   return (
-    <Button mt={mt} mb={mb} color={color} onClick={onClick}>
+    <Button mt={mt} mb={mb} className={color} onClick={onClick}>
       {children}
     </Button>
   );
@@ -12,13 +12,22 @@ const Button = styled.button`
   width: 100%;
   margin-top: ${(props) => props.mt ?? 0}px;
   margin-bottom: ${(props) => props.mb ?? 0}px;
-  background-color: var(--${(props) => props.color});
   font-size: 18px;
   font-weight: 700;
   padding: 18px 0;
   border-radius: 15px;
   box-shadow: var(--shadow);
   cursor: pointer;
+
+  &.green {
+    background-color: var(--green);
+  }
+  &.blue {
+    background-color: var(--blue);
+  }
+  &.white {
+    background-color: #ffffff;
+  }
 `;
 
 export default SquareButton;
